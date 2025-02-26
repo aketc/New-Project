be9c6ac451467d026cd23019bb21b1ae700c92c0
aketc <prisaketch@gmail.com>
2025-02-26 11:15:10 +0300

    Just a mini calculator

---
+++ index.py
@@ -0,0 +1,25 @@
+print ("Hello World!")
+#Let make mini calculator
+
+num1 = float (input("7:"))
+num2 = float(input("3:"))
+
+sum_result = 7 + 3
+difference_result = 7 - 3
+product_result = 7 * 3
+quotient_result = 7/3
+
+
+print(f"results:")
+print(f"sum: {sum_result}")
+print(f"difference: {difference_result}")
+print(f"product:{product_result}")
+print(f"quotient:{quotient_result}")
+
+
+
+
+
+
+
+       
